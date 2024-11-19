2816065bc2c0ce74a5b54701b6db2bc2dc173656
sushigadaisuki <osushi>
2024-11-20 02:30:59 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# This is ...
+Chat on ChatGPT Web can be automated.  
+You can send chats from the console.  
+
+![](./imgs/img1.gif)
+
+# Install
+1. Install tampermonky (This plugin work on tampermonkey)  
+    ref: https://chromewebstore.google.com/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo
+2. Install this plugin  
+    ref: [cgpt_automate.user.js](./cgpt_automate.user.js)
+
+# How to use
+Functions:
+* cgpt.chat
+* cgpt.chatBulk
+* cgpt.callWhenChatAvailable
+* cgpt.isChatAvailable
+
+## chat: Send chat only once
+example
+```javascript
+cgpt.chat("hello");
+```
+
+## chatBulk: Send multiple chats at once
+example
+```javascript
+cgpt.chatBulk(["hello","nice to meet you","howdy?"]);
+```
+
+## callWhenChatAvailable: Register a callback
+Register a callback function to be called when chatting is enabled.  
+The function takes chat as an argument.  
+
+example
+```javascript
+let i = 0;
+cgpt.callWhenChatAvailable(chat =>{
+    chat(`hello ${i} times`);
+    i++;
+});
+```
+
+## isChatAvailable: Check is chat available
+```javascript
+console.log(cgpt.isChatAvailable());
+```
---
+++ cgpt_automate.user.js
@@ -0,0 +1,66 @@
+// ==UserScript==
+// @name         chatGPT automation
+// @namespace    http://tampermonkey.net/
+// @version      2024-11-17
+// @description  If you want to automate chat with chatGPT-web, try this !
+// @author       sushigadaisuki
+// @match        https://chatgpt.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=chatgpt.com
+// @grant        none
+// ==/UserScript==
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(() => {
+    let isChatAvailable = () => !document.querySelector("#composer-background > div > button");
+
+    let chat = async function (text) {
+        const chatInput = document.querySelector("#prompt-textarea");
+        if (chatInput) {
+            let new_element = document.createElement('p');
+            new_element.textContent = text;
+            chatInput.removeChild(chatInput.firstChild);
+            chatInput.appendChild(new_element);
+            chatInput.dispatchEvent(new Event('input', { bubbles: true }));
+        } else {
+            console.error("Chatbox not found.");
+        }
+        await sleep(300);
+        const sendButton = document.querySelector("#composer-background > div > button");
+        if (sendButton) {
+            sendButton.click();
+        } else {
+            console.error("Send button not found");
+        }
+    };
+
+    let responseFinishCallBack = () => { };
+    let callWhenChatAvailable = (callback) => {
+        responseFinishCallBack = callback;
+    }
+
+    let chatBulkArr = [];
+    let chatBulk = (bulk) => {
+        chatBulkArr = bulk
+    }
+
+    setInterval(() => {
+        if (isChatAvailable()) {
+            if (chatBulkArr.length > 0) {
+                let txt = chatBulkArr.shift();
+                chat(txt);
+            } else {
+                responseFinishCallBack(chat);
+            }
+        }
+    }, 500);
+
+    window.cgpt = {
+        chat: chat,
+        chatBulk: chatBulk,
+        callWhenChatAvailable: callWhenChatAvailable,
+        isChatAvailable: isChatAvailable
+    };
+})();
